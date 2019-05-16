0cce99d7dea1ae92c4b737e1f7ff681601dda6fd
masukgungor <masukgungor@users.noreply.github.com>
2019-05-17 02:21:24 +0300

    Chapter0101.java

---
+++ Cahpter0101.java
@@ -0,0 +1,10 @@
+package chapter01;
+
+public class Cahpter0101 {
+	public static void main(String [] args) {
+		System.out.println("Java is good");
+		System.out.println("Java is good");
+		System.out.println("Java is good");
+	}
+
+}
